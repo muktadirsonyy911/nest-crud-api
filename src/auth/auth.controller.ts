62698c3772eab2f442212bfc953a7dcf888ce60f
Muktadir Sony <muktadir.hossain@apsissolutions.com>
2023-06-13 20:05:21 +0600

    added prisma module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController{
+    // authService: AuthService;
+    // constructor(authService: AuthService){
+    //     this.authService = authService;
+    // }
+    constructor(private authService: AuthService){}
+    
+    @Post('signUp')
+    signUp(){
+        return this.authService.signUp();
+    }
+
+    @Post('login')
+    login(){
+        return this.authService.login();
+    }
+
+} 
